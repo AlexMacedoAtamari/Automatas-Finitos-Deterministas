aeb583a53ae7948c5e39d43dc48bb8a66cca41b3
AlexMacedoAtamari <amacedoa@est.unap.edu.pe>
2026-06-13 17:22:38 -0500

    Se agregó SFML
    Se agregó la librería SFML (formato) para los gráficos que se mostrarán

--- Atomatas_Finitos_Deterministas_TC/main.cpp
+++ Atomatas_Finitos_Deterministas_TC/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+using namespace sf;
 
 int main()
 {
-    cout << "Listo para iniciar el trabajo" << endl;
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
